721a392f9b0ad5cfafe6423ff81d749509df7e33
Victoria Cepeda <142287614+VictoriaCepedaMarquinez@users.noreply.github.com>
2024-09-27 18:08:46 -0300

    se agrega card free, nav y footer

---
+++ Etregable2/script.js
@@ -0,0 +1,33 @@
+const track = document.querySelector('.carousel');
+const gameCards = document.querySelectorAll('.cardFree');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const sectionGames = document.querySelector('.games-section')
+
+let currentIndex = 0;
+
+// Calcula el ancho de cada tarjeta más el margen
+const cardWidth = gameCards[0].offsetWidth + parseFloat(getComputedStyle(gameCards[0]).marginRight);
+
+// Número de tarjetas visibles según el ancho del contenedor del carousel
+const visibleCards = Math.floor(document.querySelector('.carousel-track').offsetWidth / cardWidth);
+
+// Número total de tarjetas en el carousel
+const totalCards = gameCards.length;
+
+// Último índice alcanzable
+const maxIndex = totalCards - visibleCards;
+
+nextButton.addEventListener('click', () => {
+    if (currentIndex < maxIndex) {
+        currentIndex++;
+        track.style.transform = `translateX(-${cardWidth * currentIndex}px)`;
+    }
+});
+
+prevButton.addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+        track.style.transform = `translateX(-${cardWidth * currentIndex}px)`;
+    }
+});
